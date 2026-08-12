4c55b9683a40dd9be702163eea62aa1304d3aebc
Udit Jayan <uditjayan7910@gmail.com>
2026-08-13 02:30:27 +0530

    added a function to save the ID of tweet posted

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ CACHE_FILE = "posted_cache.json"
 RSS_URL = "https://www.google.com/alerts/feeds/14870177641225663263/4783581204708488671"
 
 '''To load previous tweet id's from local cache to prevent duplicates, after the script is run
-#all the variables are erased from memmory and when run again, bot ends up fetching the same 
+all the variables are erased from memmory and when run again, bot ends up fetching the same 
 rss items'''
 
-def load_posted_id():
+def load_posted_ids():
     if os.path.exists(CACHE_FILE):
         with open(CACHE_FILE, "r") as f:
             try:
@@ -31,3 +31,13 @@ def load_posted_id():
             except json.JSONDecoder:
                 return set()
     return set()
+
+'''To save newly tweeted ID into the cache file so that the bot dosent tweet the same thing
+again'''
+def save_posted_id(entry_id):
+    posted = load_posted_ids()
+    posted.add(entry_id)
+    with open(CACHE_FILE, "w") as f:
+        #json files cant serialize set directly, so we convert it to list, indent is just
+        #space between each entry
+        json.dump(list(posted),f,indent=2)
