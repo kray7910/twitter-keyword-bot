f5ca36d2d233b516a9e8ddda5b46cd0567639132
Udit Jayan <uditjayan7910@gmail.com>
2026-08-12 23:30:28 +0530

    added RSS url and cache file and a function to load already posted tweets

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-import time
+import feedparser
+import json
 import tweepy
 import streamlit as st
 from dotenv import load_dotenv
@@ -11,72 +12,22 @@ client = tweepy.Client(
     consumer_key = os.getenv("api_key"),
     consumer_secret = os.getenv("api_secret"),
     access_token = os.getenv("access_token"),
-    access_token_secret = os.getenv("access_token_secret")
+    access_token_secret = os.getenv("access_token_secret"),
+    wait_on_rate_limit = True
 )
 
-try:
-    me = client.get_me()
-    user_id = me.data.id
-    print(f"Logged in as @{me.data.username} (ID: {user_id})\n")
-except Exception as e:
-    print(f"Authentication error: {e}")
-    exit(1)
+CACHE_FILE = "posted_cache.json"
+RSS_URL = "https://www.google.com/alerts/feeds/14870177641225663263/4783581204708488671"
 
-keyword = "Artificial Intelligence"
-reply = "Great post! Thank you for sharing"
-max_results = 10
+'''To load previous tweet id's from local cache to prevent duplicates, after the script is run
+#all the variables are erased from memmory and when run again, bot ends up fetching the same 
+rss items'''
 
-def run_bot():
-    print(f"Searcing recent tweets for: {keyword}")
-    query = f"{keyword} -is:retweet"
-
-    try:
-        response = client.search_recent_tweets(
-            query = query,
-            max_results = max_results,
-            tweet_fields = ['author_id', 'created_at']
-        )
-        if not response.data:
-            print("No matching tweets")
-            return
-        for tweet in response.data:
-            tweet_id = tweet.id
-            author_id = tweet.author_id
-
-            if author_id == user_id:
-                continue
-            print(f"Processing tweet id: {tweet_id}")
-
+def load_posted_id():
+    if os.path.exists(CACHE_FILE):
+        with open(CACHE_FILE, "r") as f:
             try:
-                client.like(tweet_id)
-                print("Tweet liked")
-            except tweepy.TweepyException as e:
-                print(f"Like failed: {e}")
-
-            try:
-                client.retweet(tweet_id)
-                print("retweeted")
-            except tweepy.TweepyException as e:
-                print(f"Retweet failed: {e}")
-
-            try:
-                client.create_tweet(
-                    text = reply,
-                    in_reply_to_tweet_id = tweet_id
-                )
-                print("Replied")
-            except tweepy.TweepyException as e:
-                print(f"Reply failed: {e}")
-
-            time.sleep(10)
-
-    except tweepy.TweepyException as e:
-        print(f"API search error{e}")
-
-if __name__  == "__main__":
-    try:
-        me = client.get_me()
-        print(f"SUCCESS: Authenticated as @{me.data.username}")
-    except Exception as e:
-        print(f"FAILED at get_me(): {e}")
-    run_bot()
+                return set(json.load(f))
+            except json.JSONDecoder:
+                return set()
+    return set()
